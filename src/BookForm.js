63788c7179b2b8c0a2696f9412b0639917a7946b
zahrabola <95353397+zahrabola@users.noreply.github.com>
2023-12-03 15:04:47 +0000

    Update Book Form

--- src/BookForm.js
+++ src/BookForm.js
@@ -1,14 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Form } from 'react-bootstrap';
 
-const BookForm = () => {
+const BookForm = (props) => {
+    const  [book, setBook] = useState(() => {
+        return {
+            bookname: props.book ? props.book.bookname : '',
+            author: props.book ? props.book.author : '',
+            quantity: props.book ? props.book.quantity : '',
+            price: props.book ? props.book.price : '',
+            date: props.book ? props.book.date : ''
+        }
+    })
+
+    const [errormessage, setErrorMessage] = useState('');
+    const {bookname, author, price, quantity} = book
 
 
+    const handleChangeInput = (event) => {
+        const {name, value} = event.target;
+        switch(name){
+            case 'quantity':
+                if(value === '' || parseInt(value) ===+value){
+                    setBook((prevState) => ({
+                        ...prevState,
+                        [name]: value
+                    }))
+                }
+                break;
+                case 'price':
+                    if (value === '' || value.matchh(/^\d{1,}(\.\d{0,2})?$/)){
+                        setBook((prevState) => ({
+                        ...prevState,
+                        [name]: value
+                        }))
+
+                    }
+                    break;
+                    default:
+                        setBook((prevState) => ({
+                            ...prevState,
+                            [name]: value
+                        }));
+
+        }
+    }
+    
     return (
         <div>
       
             <div className='mainform'>
-                <Form>
+                <Form >
                     <Form.Group controlId='name'>
                         <Form.Label>Book name</Form.Label>
                         <Form.Control
@@ -16,6 +57,8 @@ const BookForm = () => {
                             type="text"
                             name="bookname"
                             placeholder="Enter name of book"
+                            value={bookname}
+                            onChange={handleChangeInput}
                           />
                     </Form.Group>
                     <Form.Group controlId='author'>
@@ -25,6 +68,8 @@ const BookForm = () => {
                             type="text"
                             name="author"
                             placeholder="Enter name of author"
+                            value={author}
+                            onChange={handleChangeInput}
                           />
                     </Form.Group>
                     <Form.Group controlId='quanity'>
@@ -34,6 +79,8 @@ const BookForm = () => {
                             type="quantity"
                             name="quantity"
                             placeholder="Enter available quantity"
+                            value={quantity}
+                            onChange={handleChangeInput}
                           />
                     </Form.Group>
                     <Form.Group controlId='price'>
@@ -42,6 +89,8 @@ const BookForm = () => {
                             type="text"
                             name="price"
                             placeholder="Enter price of book"
+                            value={price}
+                            onChange={handleChangeInput}
                             > 
                         </Form.Control>
                         <Button type='submit' className='submitbtn'>
