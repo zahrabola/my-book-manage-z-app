6c37b5fdb1776cb80b5bcc3a3df84bd0cd8af941
zahrabola <95353397+zahrabola@users.noreply.github.com>
2023-12-11 19:27:46 +0000

    update

---
+++ src/Hook/UseStorageLocal.js
@@ -0,0 +1,19 @@
+import { useState, useEffect } from 'react';
+
+const UseStorageLocal = (key, initialValue) => {
+    const [value, setValue] = useState (() => {
+        try {
+            const localValue = window.localStorage.getItem(key);
+            return localValue ? JSON.parse(localValue) : initialValue;
+        } catch (error){
+            return initialValue
+        }
+    });
+    useEffect(() =>{
+        window.localStorage.setItem(key, JSON.stringify(value));
+    }, [key, value]);
+    return [value, setValue]
+  
+}
+
+export default UseStorageLocal;
